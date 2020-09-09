aa59958715e107ff4e215ae744c4e99e4e658abf
Gabrielle LaMarr LeMee <lamarrlemee@gmail.com>
2020-09-09 09:03:25 -0400

    add the latest stories feed to the bottom of the page

--- src/js/App.vue
+++ src/js/App.vue
@@ -21,6 +21,37 @@
   var copy = require("./data/copy.docs.txt");
   copy = archieml.load(copy);
   var Graphic = require('./components/Graphic.vue');
+
+  const RSS_URL = `https://detroit.chalkbeat.org/rss/front-page/index.xml`;
+
+  fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    const items = data.querySelectorAll("entry");
+    var articleGroup = document.createElement("Div");
+    var articleHeader = document.createElement("H3");
+    articleHeader.innerHTML = "The Latest";
+    articleGroup.appendChild(articleHeader);
+    articleGroup.classList.add("article-group");
+
+    for(var i=0; i < 5; i++){
+      var el = items[i];
+      console.log(el);
+      var article = document.createElement("Article");
+      var title = document.createElement("A");
+      var byline = document.createElement("Div");
+      title.innerHTML = el.querySelector("title").innerHTML;
+      title.href = el.querySelector("id").innerHTML;
+      // byline.innerHTML = "By" + el.querySelector("author").innerHTML;
+      article.appendChild(title);
+      // article.appendChild(byline);
+      articleGroup.appendChild(article);
+    }
+
+    document.getElementById("app").appendChild(articleGroup);
+  });
+
   module.exports =  {
     name: 'app',
     data(){ return {
